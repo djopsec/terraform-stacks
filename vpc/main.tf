9e692b67f3d6d95aec8f90a6ab58b3ebef94d317
Nicole Bushong <nbushong@JumpClouds-MacBook-Pro-4.local>
2022-12-06 13:02:46 -0600

    tweak to work

--- vpc/main.tf
+++ vpc/main.tf
@@ -14,29 +14,31 @@ provider "aws" {
 }
 
 locals {
-  name   = "demo"
-  region = "us-east-1"
+  name   = var.vpc_name
+  region = var.region
+  cidr   = var.CIDR
 
   tags = {
-    Example    = local.name
-    GithubRepo = "terraform-stacks"
-    GithubOrg  = "djopsec"
+    Workspace  = "${terraform.workspace}"
+    GithubRepo = "${var.repo}"
+    GithubOrg  = "${var.org}"
   }
 }
 
 ################################################################################
 # VPC Module
 ################################################################################
+
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws//examples/simple-vpc"
+  source  = "terraform-aws-modules/vpc/aws"
   version = "3.18.1"
 
   name = local.name
-  cidr = "10.0.0.0/16"
+  cidr = "${local.cidr}.0.0/16"
 
   azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["${local.cidr}.1.0/24", "${local.cidr}.2.0/24", "${local.cidr}.3.0/24"]
+  public_subnets  = ["${local.cidr}.101.0/24", "${local.cidr}.102.0/24", "${local.cidr}.103.0/24"]
 
   enable_ipv6 = true
 
@@ -44,12 +46,12 @@ module "vpc" {
   single_nat_gateway = true
 
   public_subnet_tags = {
-    Name = "overridden-name-public"
+    Name = "${local.name}"
   }
 
   tags = local.tags
 
   vpc_tags = {
-    Name = local.name
+    Name = "${local.name}"
   }
 }
