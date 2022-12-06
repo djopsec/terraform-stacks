763e34a8c98c907e2f88b6d4efee2a4b201e91c5
Nicole Bushong <nbushong@JumpClouds-MacBook-Pro-4.local>
2022-12-06 10:19:23 -0600

    terraform-aws-modules/vpc/simple-vpc

---
+++ vpc/main.tf
@@ -0,0 +1,55 @@
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "terraform-stacks-demo"
+
+    workspaces {
+      name = "demo-vpc"
+    }
+  }
+}
+
+provider "aws" {
+  region = local.region
+}
+
+locals {
+  name   = "demo"
+  region = "us-east-1"
+
+  tags = {
+    Example    = local.name
+    GithubRepo = "terraform-stacks"
+    GithubOrg  = "djopsec"
+  }
+}
+
+################################################################################
+# VPC Module
+################################################################################
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws//examples/simple-vpc"
+  version = "3.18.1"
+
+  name = local.name
+  cidr = "10.0.0.0/16"
+
+  azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_ipv6 = true
+
+  enable_nat_gateway = false
+  single_nat_gateway = true
+
+  public_subnet_tags = {
+    Name = "overridden-name-public"
+  }
+
+  tags = local.tags
+
+  vpc_tags = {
+    Name = local.name
+  }
+}
