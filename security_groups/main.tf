41313070808d82e3d11eb6f77a9b406c59734a43
Nicole Bushong <nbushong@JumpClouds-MacBook-Pro-4.local>
2022-12-07 13:51:26 -0600

    public and private sg

--- security_groups/main.tf
+++ security_groups/main.tf
@@ -58,3 +58,26 @@ resource "aws_security_group" "public_https" {
 
   tags = local.tags
 }
+
+resource "aws_security_group" "all_private" {
+  name        = "all_private"
+  description = "Allow inbound outbound traffic internally in private subnets"
+  vpc_id      = local.vpc_id
+
+  ingress {
+    description = "All oppened in private subnets"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["${local.cidr}.1.0/24", "${local.cidr}.2.0/24", "${local.cidr}.3.0/24"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.tags
+}
