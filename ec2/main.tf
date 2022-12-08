c25e82dce4040ce444b4115801cb006499fe9123
Nicole Bushong <nbushong@JumpClouds-MacBook-Pro-4.local>
2022-12-08 15:39:41 -0600

    update output

--- ec2/main.tf
+++ ec2/main.tf
@@ -40,7 +40,7 @@ locals {
   region = data.terraform_remote_state.vpc.outputs.vpc_region
   cidr   = data.terraform_remote_state.vpc.outputs.vpc_cidr
   private_sg = data.terraform_remote_state.sg.outputs.all_private_security_group_id
-  private_subnets = data.terraform_remote_state.vpc.private_subnets
+  private_subnets = data.terraform_remote_state.vpc.outputs.private_subnets
 
   tags = {
     Workspace  = "${terraform.workspace}"
